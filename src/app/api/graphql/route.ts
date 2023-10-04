9a1c1696a1de2951be73e9a72221acfc279ad770
Abdifatah Bashi <abdifatah.bashi@nav.no>
2023-10-04 12:35:55 +0200

    Legger mock vedtakResponse

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,16 +1,37 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
 import { gql } from 'graphql-tag';
+import { vedtakResponse } from './mock/vedtakResponse';
+
 
 const resolvers = {
   Query: {
     hello: () => 'world',
+    vedtak: () => vedtakResponse
   },
 };
 
 const typeDefs = gql`
+  type Vedtak {
+    rammer: [Ramme]
+    utbetalinger: [Utbetaling]
+  }
+
+  type Ramme {
+    vedtakId: String
+    virkningsdato: String
+  }
+
+  type Utbetaling {
+    vedtakId: String
+    fraOgMed: String
+    tilOgMed: String
+    sumUtbetalt: Int
+  }
+
   type Query {
     hello: String
+    vedtak: Vedtak
   }
 `;
 
