141df75b06f74b6aaebeb5510ece9d3ddc10bbd7
Abdifatah Bashi <abdifatah.bashi@nav.no>
2023-10-04 11:02:02 +0200

    Fiks routing av isalive og isready api

--- src/app/api/internal/isready.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-function isReadyHandler(req: NextApiRequest, res: NextApiResponse<string>): void {
-  res.status(200).json("Ready");
-}
-
-export default isReadyHandler;
