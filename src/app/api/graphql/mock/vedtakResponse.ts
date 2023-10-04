9b9ef5a21baf3ff153302f9771cdd13380e3905d
Abdifatah Bashi <abdifatah.bashi@nav.no>
2023-10-04 12:35:55 +0200

    Legger mock vedtakResponse

---
+++ src/app/api/graphql/mock/vedtakResponse.ts
@@ -0,0 +1,27 @@
+
+export const vedtakResponse = {
+  rammer: [
+    {
+      vedtakId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      virkningsdato: "2021-01-01",
+    },
+    {
+      vedtakId: "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+      virkningsdato: "2021-01-15",
+    },
+  ],
+  utbetalinger: [
+    {
+      vedtakId: "3fa85f64-5717-4562-b3fc-2c963f66afa8",
+      fraOgMed: "2021-01-01",
+      tilOgMed: "2021-01-15",
+      sumUtbetalt: 12000,
+    },
+    {
+      vedtakId: "3fa85f64-5717-4562-b3fc-2c963f66afa9",
+      fraOgMed: "2021-01-16",
+      tilOgMed: "2021-01-30",
+      sumUtbetalt: 8000,
+    },
+  ],
+};
